f4c8234504e2346d427cea087e10820ac79d4e61
dariakoval <dasharibkavmore666@mail.ru>
2023-11-11 19:31:34 +0400

    create ResourceNotFoundException.java

---
+++ src/main/java/hackathon/code/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package hackathon.code.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
