c8593672aa999ad0b623468438f74d3c3d249684
dariakoval <dasharibkavmore666@mail.ru>
2023-11-12 01:05:57 +0400

    add swagger annotations

--- src/main/java/hackathon/code/controller/LeaderController.java
+++ src/main/java/hackathon/code/controller/LeaderController.java
@@ -3,6 +3,11 @@ package hackathon.code.controller;
 import hackathon.code.dto.LeaderDTO;
 import hackathon.code.mapper.LeaderMapper;
 import hackathon.code.repository.LeaderRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +21,7 @@ import java.util.List;
 
 import static hackathon.code.controller.LeaderController.LEADER_CONTROLLER_PATH;
 
+@Tag(name = "Users controller", description = "Interaction with leaders")
 @RestController
 @RequestMapping("${base-url}" + LEADER_CONTROLLER_PATH)
 public class LeaderController {
@@ -28,6 +34,10 @@ public class LeaderController {
     @Autowired
     private LeaderMapper leaderMapper;
 
+    @Operation(summary = "Get list of all leaders")
+    @ApiResponse(responseCode = "200", description = "List of all leaders",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = LeaderDTO.class)) })
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<LeaderDTO>> index() {
